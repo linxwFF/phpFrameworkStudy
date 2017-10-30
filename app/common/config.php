8f95fb6434ccdcb91dac70011fdd84adb65128b7
USER-20160806QE\Administrator <874226876@qq.com>
2017-10-30 18:25:38 +0800

    添加数据库文件

--- app/common/config.php
+++ app/common/config.php
@@ -6,14 +6,15 @@ return [
         'host' => '127.0.0.1',
         'user' => 'root',
         'pass' => '',
-        'dbname' => 'itcase_bxg',
+        'dbname' => 'phpFrameworkStudy',
         'port' => '3306',
+
         'charset' => 'utf8',
-        'prefix' => 'bxg_',     //前缀
+        'prefix' => 't_',     //前缀
     ],
     //SESSION的相关配置
     'SESSION' => [
-        'SESSION_PREFIX' => 'bxg',
+        'SESSION_PREFIX' => 't',
         'PHPSESSID_HTTPONLY' => true,   //保存在Cookie中PHPSESSID是否使用HttpOnly
     ],
 ];
